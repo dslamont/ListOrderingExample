4a6d56a13ca2866af5371e9a307971b389932017
Don Lamont <5798888+dslamont@users.noreply.github.com>
2020-11-27 19:33:41 +0000

    Add project files.

---
+++ ListOrderingExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ListOrderingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to exit.");
+
+            Console.ReadKey();
+        }
+    }
+}
